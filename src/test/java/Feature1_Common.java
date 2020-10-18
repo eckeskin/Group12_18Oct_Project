9a90ac48538df41e0d3b15f6fd81008d29e61a68
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-18 14:19:46 +0200

    first commit

--- src/test/java/Feature1_Common.java
+++ src/test/java/Feature1_Common.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Feature1_Common {
+public class Feature1_Common {
+    public static void main(String[] args) {
+        System.out.println("Common feature");
+    }
+
 }
