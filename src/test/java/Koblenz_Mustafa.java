e3d40d1e1690c19562bb1f97de0d664cdebefe8d
Mustafa-Gol <mustafa.gol@hotmail.com>
2020-10-18 14:54:05 +0200

    erste Kommentar

--- src/test/java/Koblenz_Mustafa.java
+++ src/test/java/Koblenz_Mustafa.java
@@ -1,7 +1,10 @@
 public class Koblenz_Mustafa {
     public static void main(String[] args) {
         System.out.println("Ich wohne in Koblenz. Die Stadt liegt am Rhein");
+<<<<<<< HEAD
 
         System.out.println("Das ist zweite Reihe nach dem ersten Kommentar");
+=======
+>>>>>>> erste Kommentar
     }
 }
