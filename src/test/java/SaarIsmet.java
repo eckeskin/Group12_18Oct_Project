b732449f7ef9e45e9ccd7ba0ee54c99be497e515
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-18 14:48:57 +0200

    Added first java file

---
+++ src/test/java/SaarIsmet.java
@@ -0,0 +1,12 @@
+public class SaarIsmet {
+    public static void main(String[] args) {
+
+        int num1 = 10, num2=20, sum;
+
+        sum = num1 + num2;
+
+        System.out.println("sum = " + sum);
+
+
+    }
+}
