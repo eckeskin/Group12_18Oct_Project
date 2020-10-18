aafa5cc5bfa207f8842040529f2931999de6934d
Caglar Keskin <caglarkeskin@gmail.com>
2020-10-18 14:33:08 +0200

    6th commit

---
+++ src/test/java/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Before second commit");
+
+        //after gitignore
+
+        //after recall from gitignore
+
+        //again gitignore
+
+
+
+    }
+}
